916030bd2e66514a61dcfd975c0294b01baf653b
Jacky Huang <jacky.huang@insight.com>
2021-09-25 16:03:44 +1000

    update main.js

--- .github/actions/hello-world/main.js
+++ .github/actions/hello-world/main.js
@@ -1,13 +1,11 @@
-const core = require("@actions/core")
+const core = require("@actions/core");
 
 const firstGreeting = core.getInput("first-greeting");
 const secondGreeting = core.getInput("second-greeting");
-const thirdGreeting = core.getInput("third-greeting")
+const thirdGreeting = core.getInput("third-greeting");
 
 console.log(`Hello ${firstGreeting}`);
 console.log(`Hello ${secondGreeting}`);
 if (thirdGreeting) {
     console.log(`Hello ${thirdGreeting}`);
 }
-
-console.log("Hello World");
