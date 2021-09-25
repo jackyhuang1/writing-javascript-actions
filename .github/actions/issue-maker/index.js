2808898b0ca2c95ad3c0c66fcdaed54558e7d0e9
Jacky Huang <jacky.huang@insight.com>
2021-09-25 19:05:49 +1000

    update issue-maker

--- .github/actions/issue-maker/index.js
+++ .github/actions/issue-maker/index.js
@@ -8,6 +8,8 @@ async function run() {
     const token = core.getInput("repo-token");
 
     const octokit = github.getOctokit(token);
+    
+    console.log("kit=", octokit);
 
     const newIssue = await octokit.issues.create({
         repo: github.context.repo.repo,
